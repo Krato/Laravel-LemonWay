5bd6ef4335b7eb6bdd9febdb2c1fad76553bdcf6
Eric Lagarda <eric@infinety.es>
2017-05-03 15:52:05 +0200

    Some visual fixes

--- src/LemonWay.php
+++ src/LemonWay.php
@@ -57,11 +57,11 @@ class LemonWay
 
     public function __construct()
     {
-        $this->apiKey = config('lemonway.api_url');
-        $this->login = config('lemonway.login');
-        $this->password = config('lemonway.password');
-        $this->language = config('lemonway.language');
-        $this->version = config('lemonway.version');
+        $this->apiKey    = config('lemonway.api_url');
+        $this->login     = config('lemonway.login');
+        $this->password  = config('lemonway.password');
+        $this->language  = config('lemonway.language');
+        $this->version   = config('lemonway.version');
         $this->sslActive = config('lemonway.ssl');
         $this->createCredentialsData();
     }
@@ -102,28 +102,53 @@ class LemonWay
         ];
     }
 
-    public function setWalletUser($wallet, $clientMail, $clientFirstName, $clientLastName, $clientTitle, $street, $postCode, $city, $cityIso3, $phoneNumber, $mobileNumber, $birthdate, $isDebtor = null, $nationalityIso3 = null, $birthCity = null, $birthCountryIso3 = null, $payerOrBeneficiary = null, $isOneTimeCustomer = null, $isTechWallet = null)
+    /**
+     * Return a LemonUser to use with api calls
+     *
+     * @param $wallet
+     * @param $clientMail
+     * @param $clientFirstName
+     * @param $clientLastName
+     * @param $clientTitle
+     * @param $street
+     * @param $postCode
+     * @param $city
+     * @param $cityIso3
+     * @param $phoneNumber
+     * @param $mobileNumber
+     * @param $birthdate
+     * @param $isDebtor
+     * @param null $nationalityIso3
+     * @param null $birthCity
+     * @param null $birthCountryIso3
+     * @param null $payerOrBeneficiary
+     * @param null $isOneTimeCustomer
+     * @param null $isTechWallet
+     *
+     * @return mixed
+     */
+    public function setWalletUser($wallet, $clientMail, $clientFirstName, $clientLastName, $clientTitle, $street = null, $postCode = null, $city = null, $cityIso3 = null, $phoneNumber = null, $mobileNumber = null, $birthdate = null, $isDebtor = null, $nationalityIso3 = null, $birthCity = null, $birthCountryIso3 = null, $payerOrBeneficiary = null, $isOneTimeCustomer = null, $isTechWallet = null)
     {
-        $user = new LemonWayUser();
-        $user->wallet = $wallet;
-        $user->clientMail = $clientMail;
-        $user->clientFirstName = $clientFirstName;
-        $user->clientLastName = $clientLastName;
-        $user->clientTitle = $clientTitle;
-        $user->street = $street;
-        $user->postCode = $postCode;
-        $user->city = $city;
-        $user->cityIso3 = $cityIso3;
-        $user->phoneNumber = $phoneNumber;
-        $user->mobileNumber = $mobileNumber;
-        $user->birthdate = $birthdate;
-        $user->isDebtor = $isDebtor;
-        $user->nationalityIso3 = $nationalityIso3;
-        $user->birthCity = $birthCity;
-        $user->birthCountryIso3 = $birthCountryIso3;
+        $user                     = new LemonWayUser;
+        $user->wallet             = $wallet;
+        $user->clientMail         = $clientMail;
+        $user->clientFirstName    = $clientFirstName;
+        $user->clientLastName     = $clientLastName;
+        $user->clientTitle        = $clientTitle;
+        $user->street             = $street;
+        $user->postCode           = $postCode;
+        $user->city               = $city;
+        $user->cityIso3           = $cityIso3;
+        $user->phoneNumber        = $phoneNumber;
+        $user->mobileNumber       = $mobileNumber;
+        $user->birthdate          = $birthdate;
+        $user->isDebtor           = $isDebtor;
+        $user->nationalityIso3    = $nationalityIso3;
+        $user->birthCity          = $birthCity;
+        $user->birthCountryIso3   = $birthCountryIso3;
         $user->payerOrBeneficiary = $payerOrBeneficiary;
-        $user->isOneTimeCustomer = $isOneTimeCustomer;
-        $user->isTechWallet = $isTechWallet;
+        $user->isOneTimeCustomer  = $isOneTimeCustomer;
+        $user->isTechWallet       = $isTechWallet;
 
         return $user;
     }
@@ -149,8 +174,8 @@ class LemonWay
         // return false;
 
         // wrap to 'p'
-        $request = json_encode(['p' => $parameters]);
-        $serviceUrl = $this->apiKey.'/'.$serviceName;
+        $request    = json_encode(['p' => $parameters]);
+        $serviceUrl = $this->apiKey . '/' . $serviceName;
 
         $headers = ['Content-type: application/json;charset=utf-8',
             'Accept: application/json',
@@ -173,17 +198,17 @@ class LemonWay
 
         $network_err = curl_errno($ch);
         if ($network_err) {
-            error_log('curl_err: '.$network_err);
+            error_log('curl_err: ' . $network_err);
             throw new Exception($network_err);
         } else {
             $httpStatus = (int) curl_getinfo($ch, CURLINFO_HTTP_CODE);
             curl_close($ch);
             if ($httpStatus == 200) {
                 $unwrapResponse = json_decode($response)->d;
-                $businessErr = $unwrapResponse->E;
+                $businessErr    = $unwrapResponse->E;
                 if ($businessErr) {
-                    error_log($businessErr->Code.' - '.$businessErr->Msg.' - Technical info: '.$businessErr->Error);
-                    throw new \Exception($businessErr->Code.' - '.$businessErr->Msg);
+                    error_log($businessErr->Code . ' - ' . $businessErr->Msg . ' - Technical info: ' . $businessErr->Error);
+                    throw new \Exception($businessErr->Code . ' - ' . $businessErr->Msg);
                 }
 
                 return $unwrapResponse;
