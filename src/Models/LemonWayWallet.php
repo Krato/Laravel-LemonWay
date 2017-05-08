2a7593693a002fc4127efa40fa200086af1c3b4e
Eric Lagarda <portalhack.es@gmail.com>
2017-05-08 12:29:41 +0000

    Apply fixes from StyleCI

--- src/Models/LemonWayWallet.php
+++ src/Models/LemonWayWallet.php
@@ -3,13 +3,11 @@
 namespace Infinety\LemonWay\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Infinety\LemonWay\LemonWayFacade as LemonWay;
 
 class LemonWayWallet extends Model
 {
     /**
      * @var array
      */
-    protected $guarded = array();
-
+    protected $guarded = [];
 }
